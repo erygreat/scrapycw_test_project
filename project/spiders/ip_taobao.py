c4fb71f9dead170bd5643a4f93987eddbcd275f2
erygreat <ery991172821@gmail.com>
2021-07-02 19:47:35 +0800

    添加包含日志的测试爬虫

---
+++ project/spiders/ip_taobao.py
@@ -0,0 +1,25 @@
+import json
+import os
+import scrapy
+
+
+class IpTaobaoSpider(scrapy.Spider):
+
+    name = 'ip_taobao'
+
+    custom_settings = {
+        'LOG_FILE': os.path.realpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../logs/ip_taobao.log'))
+    }
+
+    def start_requests(self):
+        yield scrapy.FormRequest('https://ip.taobao.com/outGetIpInfo', formdata={
+            "ip": "myip"
+        })
+
+    def parse(self, response):
+        content = json.loads(response.text)
+        if content['code'] != 0:
+            self.logger.error(content['msg'])
+            return
+        else:
+            yield content['data']
